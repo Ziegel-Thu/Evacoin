2c68bb4aef9df943dc253faeeb94f37a02f2ed80
Ziegel-Thu <zhengsr23@mails.tsinghua.edu.cn>
2024-12-29 16:00:27 +0800

    evacoin ver1  done

--- Evacoin.sol
+++ Evacoin.sol
@@ -2,11 +2,18 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";  // 引入 Ownable 合约
 
-contract MyToken is ERC20 {
-    constructor() ERC20("Evacoin", "EVA"){
+contract MyToken is ERC20, Ownable {
+    constructor() ERC20 ("Evacoin", "EVA") Ownable(msg.sender) {
         uint256 initialSupply = 500000;
         _mint(msg.sender, initialSupply * (10 ** decimals()));
     }
+
+    // 允许合约拥有者增加代币余额
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount* (10**decimals()));
+    }
 }
+
 
